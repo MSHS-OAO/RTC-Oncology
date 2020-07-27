d727b82edfba711a73c76da4f8a47dcecb75681c
armando-v <armando.villegas@mssm.edu>
2020-07-27 13:01:05 -0400

    Added DOW columns to slot usage and appt updates data

--- Data_Preprocess.R
+++ Data_Preprocess.R
@@ -4,6 +4,14 @@ library("readxl")
 
 # Data Import -------------------------------------------------------------
 
-data_LOC <- "J:/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/Service Lines/Oncology/Data/Sample Slot Usage and Appointment Update Data.xlsx"
+#data_LOC <- "J:/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/Service Lines/Oncology/Data/Sample Slot Usage and Appointment Update Data.xlsx"
+data_LOC <- "C:/Users/Armando/Documents/RTC-Data/Sample Slot Usage and Appointment Update Data.xlsx"
 slot_usage <- read_excel(data_LOC, sheet = 1)
 appt_updates <- read_excel(data_LOC, sheet = 2)
+
+
+
+# Slot Usage Pre-Process --------------------------------------------------
+slot_usage$DOW <- weekdays(as.Date(slot_usage$DATE_TIME)) #Get the DOW based off of Date_time
+appt_updates$DOW <- weekdays(as.Date(appt_updates$APPT_DATE_TIME))
+
