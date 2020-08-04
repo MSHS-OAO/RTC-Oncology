af8ed73323f0f4ad00a60268912ddfa28cc04663
armando-v <armando.villegas@mssm.edu>
2020-08-04 12:22:33 -0400

    Delete rows with NA as a resourcein slot_usage

--- Data_Preprocess.R
+++ Data_Preprocess.R
@@ -1,12 +1,13 @@
 # Packages ----------------------------------------------------------------
 library("readxl")
 library("lubridate")
+library("dplyr")
 
 
 # Data Import -------------------------------------------------------------
 
 #data_LOC <- "J:/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/Service Lines/Oncology/Data/Sample Slot Usage and Appointment Update Data.xlsx"
-data_LOC <- "C:/Users/Armando/Documents/RTC-Data/Sample Slot Usage and Appointment Update Data.xlsx"
+data_LOC <- "C:/Users/Administrator/Downloads/Sample Slot Usage and Appointment Update Data.xlsx"
 slot_usage <- read_excel(data_LOC, sheet = 1)
 appt_updates <- read_excel(data_LOC, sheet = 2)
 
@@ -18,7 +19,7 @@ slot_usage$DATE_TIME <- format(slot_usage$DATE_TIME, "%m-%d-%Y %H:%M:%S") #refom
 slot_usage$appt_dow <- weekdays(as.Date(slot_usage$DATE_TIME)) #Get the DOW based off of Date_time
 slot_usage$appt_month <- months(as.Date(slot_usage$DATE_TIME)) #get month of appt
 slot_usage$slot_hour <- hour(strptime(slot_usage$DATE_TIME, format = "%m-%d-%Y %H:%M:%S"))
-
+slot_usage <- slot_usage[!is.na(slot_usage$RESOURCE),] #deletes rows with NA as a rsource
 
 
 # Appt Updates Pre-Process ------------------------------------------------
