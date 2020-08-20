b7008e1e331a55382886e81a2982de738e1ec9e6
Villegas <armando.villegas@mssm.edu>
2020-08-20 09:35:37 -0400

    Added block comment

--- Data_Preprocess.R
+++ Data_Preprocess.R
@@ -2,34 +2,25 @@
 library("readxl")
 library("lubridate")
 library("dplyr")
-
+library("magrittr")
 
 # Data Import -------------------------------------------------------------
 
-#data_LOC <- "J:/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/Service Lines/Oncology/Data/Sample Slot Usage and Appointment Update Data.xlsx"
+slot_usage <- read_excel("J:/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/Service Lines/Oncology/Data/Cerner Actual data/RTC Slot usage jun01-aug31 2020.xlsx")
 #data_LOC <- "C:/Users/Administrator/Downloads/Sample Slot Usage and Appointment Update Data.xlsx"
-slot_usage <- read_excel("C:/Users/Armando/Downloads/Cerner Actual data/RTC Slot usage jun01-aug31 2020.xlsx")
+#slot_usage <- read_excel("C:/Users/Armando/Downloads/Cerner Actual data/RTC Slot usage jun01-aug31 2020.xlsx")
 
 
 # Slot Usage Pre-Process --------------------------------------------------
-#slot_usage <- slot_usage[order(as.Date(slot_usage$DATE_TIME, format="%Y-%m-%d")),] #sort slot_usage by date
-#slot_usage$DATE_TIME <- format(slot_usage$DATE_TIME, "%m-%d-%Y %H:%M:%S") #refomrat Date_Time
-# slot_usage$appt_dow <- weekdays(as.Date(slot_usage$DATE_TIME)) #Get the DOW based off of Date_time
-# slot_usage$appt_month <- months(as.Date(slot_usage$DATE_TIME)) #get month of appt
-# slot_usage$slot_hour <- hour(strptime(slot_usage$DATE_TIME, format = "%m-%d-%Y %H:%M:%S"))
-#slot_usage <- slot_usage[!is.na(slot_usage$RESOURCE),] #deletes rows with NA as a rsource
-# slot_usage$resource_time_comb <- paste(slot_usage$RESOURCE,slot_usage$DATE_TIME) #combine time and resource 
-# slot_usage$overbook <- duplicated(slot_usage$resource_time_comb) #find duplicates in resource_time_comb
-# slot_usage <- slot_usage[!(slot_usage$SLOT_TYPE == "Unavailable"),]
-# slot_usage <- slot_usage[!(slot_usage$SLOT_TYPE == "RTC DO NOT BOOK"),]
-# slot_usage <- slot_usage[!(slot_usage$APPT_TYPE == "EST PT-COVID19 SCREENING"),] 
-# slot_usage <- slot_usage[!(slot_usage$APPT_TYPE == "Non Face to Face"),] 
-# procedure_slot_usage <- slot_usage[(slot_usage$SLOT_TYPE == "Unavailable"),]
-# provider_slot_usage <-
 
 
+## Using mutate columns are created for the DOW,month,hour, and 
+##whether the appointment is overbooked or not
+##filter out specific slot types that we are not looking for
+##as well as remoove any NAs in slot_type or RESOURCE
+##Order the DATA by DATE_TIME and then split the data into
+##a procedure data frame and a provider data frame
 
-#mutate 
 #slot_usage <-
 slot_usage %<>% #assignment/pass
   mutate(appt_dow = wday(DATE_TIME, label = T), 
@@ -54,18 +45,9 @@ procedure_slot_usage <-
 
 provider_slot_usage <-
   slot_usage %>%
-  filter(RESOURCE %in% unique(provider_slot_usage$RESOURCE))
-
-provider_slot_usage <-
-  slot_usage %>%
-  filter(RESOURCE %in% unique(provider_slot_usage$RESOURCE))
+  filter(!RESOURCE %in% unique(procedure_slot_usage$RESOURCE))
 
 # provider_slot_usage <-
 #   slot_usage %>%
-#   filter(!RESOURCE %in%
-#            c("CSM Rutt Lab Room 4 Floor", "VAD 3 Floor", "VAD 4 Floor", "CSM Rutt Lab Room 3 Floor", "Procedure Room"))
-
-
-
-
+#   filter(RESOURCE %in% unique(provider_slot_usage$RESOURCE))
 
