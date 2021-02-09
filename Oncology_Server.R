87bae6d79bc4b39a60316771afb832f1c767f2fb
Villegas <armando.villegas@mssm.edu>
2021-02-09 15:43:05 -0500

    added observe events to the main filters in server

--- Oncology_Server.R
+++ Oncology_Server.R
@@ -1,4 +1,40 @@
 server <- function(input, output, session) {
+  observeEvent(input$selectedCampus,{
+    updatePickerInput(session,
+                      inputId = "selectedSpecialty",
+                      choices = NULL
+    )},
+    ignoreInit = TRUE)
+  
+  observeEvent(c(input$selectedCampus,input$selectedSpecialty),{
+    updatePickerInput(session,
+                      inputId = "selectedDepartment",
+                      choices = NULL
+    )},
+    ignoreInit = TRUE)
+  
+  observeEvent(c(input$selectedCampus,input$selectedSpecialty, input$selectedDepartment),{
+    updatePickerInput(session,
+                      inputId = "selectedvisitype",
+                      choices = NULL
+    )},
+    ignoreInit = TRUE)
+  
+  observeEvent(c(input$selectedCampus,input$selectedSpecialty, input$selectedDepartment,
+                    input$selectedvisitype),{
+    updatePickerInput(session,
+                      inputId = "selectedprovider",
+                      choices = NULL
+    )},
+    ignoreInit = TRUE)
+  
+  observeEvent(c(input$selectedCampus,input$selectedSpecialty, input$selectedDepartment,
+                 input$selectedvisitype, input$selectedprovider),{
+                   updatePickerInput(session,
+                                     inputId = "selectedrefprovider",
+                                     choices = NULL
+                   )},
+               ignoreInit = TRUE)
   
 }
 
