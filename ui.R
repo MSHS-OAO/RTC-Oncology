1af751496521f513b6fa9c5ad2405e47c83d3ae4
Villegas <armando.villegas@mssm.edu>
2021-07-12 17:19:05 -0400

    Update mapping file

--- ui.R
+++ ui.R
@@ -197,7 +197,7 @@ ui <- dashboardPage(
                                        p("The data used in this dashboard is pulled from the Epic Clarity database using the slot and access data tables, named CRREPORT_REP.Y_DM_BOOKED_FILLED_RATE and CRREPORT_REP.MV_DM_PATIENT_ACCESS respectively.
                                       The master file including the department, PRC, and disease group mappings as well as the LOS exclusions used in this analytics tool can be downloaded from the first hyperlink, while the zip code groupings file can be downloaded at the second link.",
                                          style = "font-size:16px"),
-                                       a(href = "Mappings/Oncology System Dashboard - Data Groupings - Saved 6.11.2021.xlsx",target='blank', 'Oncology Master Mapping File', download = 'Oncology Master Mappings.xlsx', style = "font-size:16px"),
+                                       a(href = "Mappings/Oncology System Dashboard - Data Groupings - Saved 7.9.2021.xlsx",target='blank', 'Oncology Master Mapping File', download = 'Oncology Master Mappings.xlsx', style = "font-size:16px"),
                                        br(),
                                        a(href = "Mappings/Oncology System Data - Zip Code Groupings 4.13.2021.csv",target='blank', 'Zip Code Groupings', download = 'Zip Code Groupings.csv', style = "font-size:16px")
                               ))
