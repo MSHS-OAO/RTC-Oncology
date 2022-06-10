1506da33e791c9e6eb24f52a43075e7ae3a2017e
Villegas <armando.villegas@mssm.edu>
2022-06-10 14:05:20 -0400

    add callback to global

--- global.R
+++ global.R
@@ -745,6 +745,17 @@ all_provider <- all_provider[,1]
 
 operating_hours_choices <- c("7:00AM", "7:30AM",  "8:00AM", "8:30AM", "9:00AM", "9:30AM", "10:00AM", "10:30AM", "11:00AM", "11:30AM", "12:00PM", "12:30PM", "1:00PM", "1:30PM", "2:00PM", "2:30PM", "3:00PM", "3:30PM", "4:00PM", "4:30PM", "5:00PM", "5:30PM", "6:00PM", "6:30PM")
 
+callback <- callback <- JS(
+  "var a = document.createElement('a');",
+  "$(a).addClass('dt-button');",
+  "a.href = document.getElementById('download1').href;",
+  "a.download = '';",
+  "$(a).attr('target', '_blank');",
+  "$(a).text('Download');",
+  "$('div.dwnld').append(a);",
+  "$('#download1').hide();"
+)
+
 # dept_mapping <- read_excel("www/Mappings/Oncology System Dashboard - Data Groupings - Saved 11.10.2021.xlsx", sheet = "Department Consolidaton-Filter") %>%
 #                   select(-`EPIC  Department`, -SITE, -ACTIVE) %>%
 #                   rename(Dept_Mapping = `Display in Filter as`,
