6b2332cc56b54a494ba650de20ae372702a509fc
Villegas <armando.villegas@mssm.edu>
2022-08-15 09:24:17 -0400

    Update hour choices

--- global.R
+++ global.R
@@ -146,7 +146,7 @@ all_provider <- read_excel("www/Mappings/Oncology System Dashboard - Data Groupi
 all_provider <- all_provider[,1]
 
 
-operating_hours_choices <- c("7:00AM", "7:30AM",  "8:00AM", "8:30AM", "9:00AM", "9:30AM", "10:00AM", "10:30AM", "11:00AM", "11:30AM", "12:00PM", "12:30PM", "1:00PM", "1:30PM", "2:00PM", "2:30PM", "3:00PM", "3:30PM", "4:00PM", "4:30PM", "5:00PM", "5:30PM", "6:00PM", "6:30PM")
+operating_hours_choices <- c("7:00AM", "7:30AM",  "8:00AM", "8:30AM", "9:00AM", "9:30AM", "10:00AM", "10:30AM", "11:00AM", "11:30AM", "12:00PM", "12:30PM", "1:00PM", "1:30PM", "2:00PM", "2:30PM", "3:00PM", "3:30PM", "4:00PM", "4:30PM", "5:00PM", "5:30PM", "6:00PM", "6:30PM", "7:00PM", "7:30PM", "8:00PM", "8:30PM")
 
 callback <- callback <- JS(
   "var a = document.createElement('a');",
