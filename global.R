31ea216ce9e3e9ba087602f34bcb92bc301ec388
armando-v <>
2022-11-15 10:25:11 -0500

    remove tcltk

--- global.R
+++ global.R
@@ -11,7 +11,7 @@ suppressMessages({
   library(shinyWidgets)
   library(htmlwidgets)
   library(lubridate)
-  library(tcltk)
+  #library(tcltk)
   library(tidyverse)
   library(plotly)
   library(knitr)
