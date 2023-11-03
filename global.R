683fe2f6f0935b9c2506b2c5acaca86696ee4a33
armando-v <>
2023-11-03 14:30:24 -0400

    fix variable for start date to test 2019 data

--- global.R
+++ global.R
@@ -557,10 +557,11 @@ daysOfWeek.options <- c("Mon","Tue","Wed","Thu","Fri","Sat","Sun")
 #dateRangetrend_min <- as.Date("2019-01-01")
 # dateRange_max <- max(historical.data$Appt.DateYear)
 
-dateRangetrend_min <- glue("Select min(APPT_DTTM) AS minDate FROM ONCOLOGY_ACCESS")
-dateRangetrend_min <- dbGetQuery(con, dateRangetrend_min)
-dateRangetrend_min <- as.Date(dateRangetrend_min$MINDATE, format="%Y-%m-%d")
+# dateRangetrend_min <- glue("Select min(APPT_DTTM) AS minDate FROM ONCOLOGY_ACCESS")
+# dateRangetrend_min <- dbGetQuery(con, dateRangetrend_min)
+# dateRangetrend_min <- as.Date(dateRangetrend_min$MINDATE, format="%Y-%m-%d")
 
+dateRangetrend_min <- as.Date("2021-01-01", format="%Y-%m-%d")
 
 
 
