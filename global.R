cb2606d8fd2b06956370d9927189a14e7dbf6eb3
Dheeraj Tommandru <tommad01@mssm.edu>
2026-02-20 13:32:07 -0500

    Staging Changes

--- global.R
+++ global.R
@@ -78,7 +78,7 @@ suppressMessages({
 
 source("global_functions.R")
 
-con <- dbConnect(odbc::odbc(), "OAO Cloud DB Production", timeout = 30)
+con <- dbConnect(odbc::odbc(), "OAO Cloud DB Staging", timeout = 30)
 
 oncology_tbl <- tbl(con, "ONCOLOGY_ACCESS")
 oncology_filters_tbl <- tbl(con, "ONCOLOGY_FILTERS")
